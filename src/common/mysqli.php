9f6053cea0ad0cc9b3b04cfe884c6ef4016121ed
SaviPrograms <42438445+SaviPrograms@users.noreply.github.com>
2022-11-08 14:51:17 +0000

    Update DB Credentials

--- src/common/mysqli.php
+++ src/common/mysqli.php
@@ -1,10 +1,10 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$dbHost = (getenv("DATABASE_HOST") ?? "wolinski_4a");
+$dbHost = (getenv("DATABASE_HOST") ?? "localhost");
 $dbUser = (getenv("DATABASE_USER") ?? "root");
 $dbPass = (getenv("DATABASE_PASSWORD") ?? "");
-$dbName = (getenv("DATABASE_DB") ?? "");
+$dbName = (getenv("DATABASE_DB") ?? "wolinski_4a");
 
 $mysqli = new mysqli(
     $dbHost,
